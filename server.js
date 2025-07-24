6d469eef80e00c1eff2c88bc768a45c66835f9a9
mayankFactacy <mayank@factacy.ai>
2025-07-24 18:10:27 +0530

    Updated cors error

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express()
-const port= 3000;
+const port = 3000;
 const cors = require('cors');
-const biginRoutes = require('./routes/biginRoutes');  
+const biginRoutes = require('./routes/biginRoutes');
 const logger = require('./logger')
 
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://factacy.ai", "http://localhost:3000","https://factacy.ai"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.use('/api/bigin',biginRoutes)
+app.use('/api/bigin', biginRoutes)
 
 
-app.get(`/`,(req,res)=>{
+app.get(`/`, (req, res) => {
     res.send(`3000 ok`)
 })
 
-app.listen(port,()=>{
-    logger.info( `Server is running on port: ${port}`);
+app.listen(port, () => {
+    logger.info(`Server is running on port: ${port}`);
 })
