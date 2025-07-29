43d1e467a87c58fc9e7aa0115d72c79dd55a91a0
mayank <134508591+mayankDseu@users.noreply.github.com>
2025-07-29 16:04:42 +0530

    Bigin login controller change

--- config/zohoConfig.js
+++ config/zohoConfig.js
@@ -2,8 +2,10 @@ const dotenv = require('dotenv')
 const path = require('path');
 
 dotenv.config({
-    path:path.resolve(__dirname,'.env')
+    path:path.resolve(__dirname,'../../.env')
 });
+
+
 
 
 module.exports = {
