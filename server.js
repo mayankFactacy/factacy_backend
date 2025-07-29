665f1ebede4dd986fa45abe71c7d1705bde1bfd5
mayank <134508591+mayankDseu@users.noreply.github.com>
2025-07-29 16:06:31 +0530

    update in server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 8002;
+const port = 15001;
 const cors = require('cors');
 const biginRoutes = require('./routes/biginRoutes');
 const logger = require('./logger')
